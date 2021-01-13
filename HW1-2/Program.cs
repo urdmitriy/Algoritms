8470e8c90386011184dcb147166ff259ab5e3b81
Uryupin <urdmitriy@yandex.ru>
2021-01-13 17:14:37 +0300

    HW1

---
+++ HW1-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0; // O(1)
+            for (int i = 0; i < inputArray.Length; i++) // O(N)
+            {
+                for (int j = 0; j < inputArray.Length; j++) // O(N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++) // O(N)
+                    {
+                        int y = 0; // O(1)
+
+                        if (j != 0) 
+                        {
+                            y = k / j; // O(1)
+                        }
+
+                        sum += inputArray[i] + i + k + j + y; // O(1)
+                    }
+                }
+            }
+
+            return sum; // O(1)
+        }
+        // Итого: O(1+N*N*N*(1+1+1) = O(1+3N^3) = O(N^3) 
+    }
+}
---
+++ HW1-3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для какого числа нужно рассчитать число Фибонначи?");
+            float num;
+            float.TryParse(Console.ReadLine(), out num);
+
+            Console.WriteLine($"Рекурсивный метод: {FibRecurse(num)}");
+            Console.WriteLine($"Метод без рекурсии: {FibFor(num)}");
+
+            Console.ReadLine();
+        }
+
+        static float FibRecurse(float num)
+        {
+            float result;
+            if (num == 0)
+            {
+                return 0;
+            }
+
+            if (num == 1)
+            {
+                return 1;
+            }
+
+            result = FibRecurse(num - 2) + FibRecurse(num - 1);
+            return result;
+        }
+
+        static float FibFor(float num)
+        {
+            float result=1;
+            float prevNum1=0; //(n-2)
+            float prevNum2 = 1; //(n-1)
+
+            if (num == 0) return 0;
+
+            for (float i = 1; i < num; i++)
+            {
+                result = prevNum1 + prevNum2;
+                prevNum1 = prevNum2;
+                prevNum2 = result;
+            }
+            return result;
+        }
+    }
+}
