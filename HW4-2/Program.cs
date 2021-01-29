eab9d0151cd2b0f814dc01a3bd084d7413bcb53c
Uryupin <urdmitriy@yandex.ru>
2021-01-29 15:32:13 +0300

    Тестирование на удаление

--- HW4-2/Program.cs
+++ HW4-2/Program.cs
@@ -13,35 +13,36 @@ namespace HW4_2
     {
         static void Main(string[] args)
         {
-
             BinaryTree MyTree = new BinaryTree();
 
 
             MyTree.Insert(55);
             MyTree.Insert(19);
             MyTree.Insert(36);
-            //MyTree.Insert(48);
-            //MyTree.Insert(51);
-            //MyTree.Insert(15);
-            //MyTree.Insert(98);
-            //MyTree.Insert(42);
-            //MyTree.Insert(16);
-            //MyTree.Insert(44);
-            //MyTree.Insert(30);
-            //MyTree.Insert(9);
+            MyTree.Insert(48);
+            MyTree.Insert(51);
+            MyTree.Insert(15);
+            MyTree.Insert(98);
+            MyTree.Insert(42);
+            MyTree.Insert(16);
+            MyTree.Insert(44);
+            MyTree.Insert(30);
+            MyTree.Insert(9);
+
+
 
 
 
             MyTree.Print(MyTree);
 
 
-            //Console.WriteLine("============ Удаляем 48 ==========");
-            //MyTree.Delete(48);
+            Console.WriteLine("============ Удаляем 48 ==========");
+            MyTree.Delete(48);
+            MyTree.Print(MyTree);
+
+            //Console.WriteLine("============ Вращаем влево 19 ноду ==========");
+            //MyTree.rotateRight(19);
             //MyTree.Print(MyTree);
-
-            Console.WriteLine("============ Вращаем влево 19 ноду ==========");
-            MyTree.rotateRight(19);
-            MyTree.Print(MyTree);
         }
     }
 }
